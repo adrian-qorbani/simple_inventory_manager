1f5b5d14e6df27b31a8dfdf07a49b55b5543b180
Mahdi <mahdigh@live.com>
2024-04-24 11:48:48 +0330

    fix(auth-route): better naming for auth route and fixed missing route registration in index.ts.

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { generateToken } from "../controllers/authController";
 
-export default function routes(server: FastifyInstance) {
+export default function authRoutes(server: FastifyInstance) {
   server.post("/auth", generateToken);
 }
